0351da81d5f1299a4f043fb858faa500c172ad9c
Andrew Capp <acapp909@gmail.com>
2019-10-17 22:12:21 +0000

    Done.

--- lib/min.rb
+++ lib/min.rb
@@ -8,6 +8,22 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
-def get_the_min(groceries)
+def get_the_min_old(groceries)
   #code your solution here!
+  item_array = []
+    groceries.each do |food_type, items|
+      items.each do |item|
+        item_array << item
+      end
+    end
+    min_item = item_array.min
+  #binding.pry
+
+end
+
+def get_the_min(groceries)
+  #item_array = groceries.values.flatten
+
+  #min_item = item_array.min
+  min_item = groceries.values.flatten.min 
 end
--- lib/values.rb
+++ lib/values.rb
@@ -8,6 +8,20 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
-def get_the_values(groceries)
+def get_the_values_old(groceries)
   #code your solution here!
+  values_array = []
+
+  groceries.each do |food_type, items|
+    items.each do |item|
+      values_array << item
+      #binding.pry
+    end
+  end
+  values_array
+end
+
+def get_the_values(groceries)
+  groceries.values.flatten
+
 end
